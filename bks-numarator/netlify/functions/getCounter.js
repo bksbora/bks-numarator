4b10d138cb283e905621dd7e6987ab610ca44285
Bora Keçeci <123377633+bksbora@users.noreply.github.com>
2025-06-30 14:18:27 +0300

    Delete bks-numarator directory

--- bks-numarator/netlify/functions/getCounter.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-
-export async function handler() {
-  const data = JSON.parse(fs.readFileSync("./counter.json", "utf8"));
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ count: data.count }),
-  };
-}
